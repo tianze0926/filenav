35bb1fdab99634a1e1d67a9513bf4ba9e83e200c
tianze0926 <i@tianze.me>
2025-04-03 22:44:24 +0800

    fix(thumbnail): sometimes total frames is 0

--- backend/filenav/api/thumbnail.py
+++ backend/filenav/api/thumbnail.py
@@ -1,5 +1,6 @@
 import io
 
+from fastapi.logger import logger
 from fastapi.responses import StreamingResponse
 import av
 from PIL import Image
@@ -15,7 +16,22 @@ def seek(video_path: str) -> Image.Image:
         video = container.streams.video[0]
         assert video.average_rate is not None
         assert video.time_base is not None
-        frame_i = int(video.frames * PERCENTAGE)
+        total_frames = video.frames
+        if total_frames == 0:
+            # https://stackoverflow.com/questions/53365792/number-of-frames-in-a-video-using-pyav#comment136293680_58768355
+            total_frames = int(
+                video.metadata.get(
+                    "NUMBER_OF_FRAMES-eng", video.metadata.get("NUMBER_OF_FRAMES", "0")
+                )
+            )
+        if total_frames == 0:
+            logger.warning(
+                "Got total frames as zero in %s; video.frames: %s; metadata: %s",
+                video_path,
+                video.frames,
+                video.metadata,
+            )
+        frame_i = int(total_frames * PERCENTAGE)
         container.seek(int(frame_i / video.average_rate * av.time_base))
         frame = next(container.decode(video))
         for _ in range(int(frame.pts * video.time_base * video.average_rate), frame_i):
