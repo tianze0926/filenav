73164d53789f665945b3904d10f12fdcce48acfb
tianze0926 <i@tianze.me>
2025-04-22 17:06:48 +0800

    fix: http header does not support utf-8

--- backend/filenav/api/file.py
+++ backend/filenav/api/file.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from urllib.parse import quote
 from fastapi.responses import FileResponse
 
 from ..entrypoint import api
@@ -7,9 +8,10 @@ from ..entrypoint import api
 @api.get("/file")
 def file(path: str) -> FileResponse:
     path_ = Path(path)
+    filename = quote(path_.name)  # http header does not support utf-8
     return FileResponse(
         path_,
         headers={
-            "Content-Disposition": f'attachment; filename="{path_.name}"',
+            "Content-Disposition": f'attachment; filename="{filename}"',
         },
     )
